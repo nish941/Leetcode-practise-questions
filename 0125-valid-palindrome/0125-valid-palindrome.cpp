3e469b0b9198691580e7ea8d15ac8bc60035afed
Nish941 <nishitaagarwal006@gmail.com>
2025-03-18 22:14:26 +0530

    Time: 0 ms (100%), Space: 10.1 MB (41.85%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,33 @@
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string filtered;
+        for (char c : s) {
+            if (isalnum(c)) {
+                filtered += tolower(c);
+            }
+        }
+        
+        if (filtered.empty()) {
+            return true;
+        }
+        
+        int left = 0;
+        int right = filtered.size() - 1;
+        
+        while (left < right) {
+            if (filtered[left] != filtered[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+};
